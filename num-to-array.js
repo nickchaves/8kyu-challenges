0c726cbaa93a491e190f4eb00e00c2838d2097cb
Nicholas Chaves <chavesndev@gmail.com>
2023-09-11 08:01:13 -0400

    added new challenge

---
+++ num-to-array.js
@@ -0,0 +1,16 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number 
+// within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+// My solution
+function digitize (num){
+    const modNum = num.toString().split('').reverse();
+    return modNum.map(Number);
+};
+
+console.log(digitize(1243));
+console.log(digitize(8923));
